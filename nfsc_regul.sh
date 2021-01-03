3b1e649a2be54e2cd2835e165139be2fc77dfe95
Artem <18628167+A3buki@users.noreply.github.com>
2021-01-04 01:22:36 +0300

    Update nfsc_regul.sh

--- nfsc_regul.sh
+++ nfsc_regul.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-
-##
-# Скрипт для клиента nfs
-# устанавливает необходимые пакеты
-# подключает сетевой NFS ресурс.
-##
-
-yum install nfs-utils
-
-echo regul > /etc/hostname
-echo "192.168.10.10  sirius" >> /etc/hosts
-echo "192.168.10.11  regul" >> /etc/hosts
-
-# Создаем каталог для монтирования
-cd /
-sudo mkdir nfsmount
-sudo chmod 777 nfsmount
-
-sudo mount -t nfs sirius:/nfsshare /nfsmount
+sudo yum install nfs-utils -y
+sudo systemctl start rpcbind
+sudo systemctl enable rpcbind
+sudo mkdir /mnt/share
+sudo mount -t nfs -o nfsvers=3 192.168.50.10:/tmp/share /mnt/share
+echo -e "192.168.50.10:/tmp/share\t/mnt/share\tnfs\tdefaults,vers=3\t0\t0" | sudo tee -a /etc/fstab
+exit 0
